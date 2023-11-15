7112e434eee146cfbf913920f99eeee57e22d21b
KailanXiong <1786876945@qq.com>
2023-11-15 12:15:58 +0800

    add: set different figure color

--- src/views/split_diffirent_material/index.vue
+++ src/views/split_diffirent_material/index.vue
@@ -5,7 +5,7 @@
 
 <script>
 export default {
-  name: '自定义几何体'
+  name: '划分几何体材质'
 }
 </script>
 <script setup>
@@ -50,18 +50,42 @@ const geometry = () => {
 
   // 定义index集合
   const vertices = new Float32Array([
-    -1, -1, 0,1,-1,0,1,1,0,-1,1,0,1,1,1
+    -1, -1, 0,
+    1,-1,0,
+    1,1,0,
+    -1,1,0,
+    1,1,1
   ]);
   geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
   const indices = new Uint16Array([
-    0,1,2,2,3,0,2,1,4,4,0,1,4,1,3,4,3,2
+    0,1,2,
+    2,3,0,
+    2,1,4,
+    4,0,1,
+    4,3,0,
+    4,3,2
   ]);
   geometry.setIndex(new THREE.BufferAttribute(indices, 1));
-  
+
   // 定义材质
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide, wireframe: true });
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
+  const material1 = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+ 
+  // 定义两个顶点组
+  geometry.addGroup(0, 3, 0);
+  geometry.addGroup(3, 3, 1);
+  geometry.addGroup(6, 3, 0);
+  geometry.addGroup(9, 3, 1);
+  geometry.addGroup(12, 3, 0);
+  geometry.addGroup(15, 3, 1);
+ 
   // 定义网格
-  const cube = new THREE.Mesh(geometry, material);
+  const cube = new THREE.Mesh(geometry, [material, material1]);
+  scene.add(cube);
+
+
+
+
 
   new OrbitControls(camera, renderer.domElement);
 
@@ -69,7 +93,17 @@ const geometry = () => {
   const axesHelper = new THREE.AxesHelper(5);
   scene.add(axesHelper);
 
-  scene.add(cube);
+  const BoxGeometry = new THREE.BoxGeometry(1,1,1);
+  const BoxMaterial1 = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  const BoxMaterial2 = new THREE.MeshBasicMaterial({color: 0xff0000});
+  const BoxMaterial3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
+  const BoxMaterial4 = new THREE.MeshBasicMaterial({color: 0xffff00});
+  const BoxMaterial5 = new THREE.MeshBasicMaterial({color: 0x00ffff});
+  const BoxMaterial6 = new THREE.MeshBasicMaterial({color: 0xff00ff});
+
+  const Box = new THREE.Mesh(BoxGeometry, [BoxMaterial1, BoxMaterial2, BoxMaterial3, BoxMaterial4, BoxMaterial5, BoxMaterial6]);
+  Box.position.set(3,0,0);
+  scene.add(Box);
 
 
   const animate = function () {
